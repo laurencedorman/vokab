1d739885ca12af356e9ab80ee12516e057723886
Laurence DORMAN <laurence.dorman@manomano.com>
2019-04-16 14:29:51 +0200

    add: absolute most basic functionality of a vocab memory exercise

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const initialTerms = [{
+  foreign: 'bleu',
+  native: 'blue',
+},{
+  foreign: 'voiture',
+  native: 'car',
+},{
+  foreign: 'brosse',
+  native: 'brush',
+},{
+  foreign: 'faire',
+  native: 'to do',
+}];
+
+const getCurrentTerm = (terms) => {
+  const filtered = terms.filter(term => !term.hasOwnProperty('matched'));
+
+  return filtered[Math.floor(Math.random() * filtered.length)];
 }
+
+const App = () => {
+  const [terms, setTerms] = useState(initialTerms);
+  const matchTerm = (currentTerm, toCheck) => {
+    if (currentTerm.foreign === toCheck.foreign && toCheck.native === currentTerm.native) {
+      setTerms(prevTerms => prevTerms.map(term => {
+        if (term.foreign === toCheck.foreign) {
+          term.matched = true;
+        }
+        
+        return term;
+      }));
+    }
+  };
+
+  const currentTerm = getCurrentTerm(terms);
+
+  if (!currentTerm) {
+    return (<div>Done!</div>);
+  }
+  
+  return (
+    <div className="App">
+        {<div>{currentTerm.foreign}</div>}
+        {terms.map(term =>
+          <button
+            key={term.foreign}
+            type="button"
+            disabled={term.matched}
+            onClick={() => { matchTerm(currentTerm, term ) }}
+          >
+            {term.native}
+          </button>
+        )}
+    </div>
+  );
+};
 
 export default App;
